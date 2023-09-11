f674c3ce20fe64d8a3657d24bc91f0f9f91d1395
benjizhai <ben@zhai.ch>
2023-09-11 07:58:24 +0200

    doc
    Signed-off-by: benjizhai <ben@zhai.ch>

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,35 @@
+#![doc = include_str!("../README.md")]
+
 extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
 //use syn::punctuated::Punctuated;
 use syn::{parse_macro_input, AttributeArgs, ItemFn, NestedMeta};
 
+/// Macro used to add guards to functions. If the guard function returns `Ok` then execution would continue.
+/// If the guard function returns `Err` the execution would stop and state changes would be reverted by invoking `ic_cdk::api::call::reject`.
+/// 
+/// If multiple guard functions exist they're executed in the order declared.
+/// 
+/// Usually the `modifiers` macro should be the last macro declared for a function, below everything else such as `update` and `query`.
+/// Those are the only macros tested to work together with this `modifiers` macro. 
+/// If you would like to use it with other macros or use a different order, remember Rust macro ordering rules apply, and always check the expanded result. 
+/// 
+/// The signatures of guard functions must be of
+/// ```
+/// fn func_name(params...) -> Result<(), String>
+/// ```
+/// Then the guard functions can be added as modifiers using the follow syntax:
+/// ```
+/// #[modifiers("func_name@params", ...)]
+/// ```
+/// The `params` is a commma-separated list(without spaces in between) of the arguments.
+/// # Examples
+/// ```
+/// #[modifiers("guard_func")]
+/// #[modifiers("guard_func@42,SomeEnum::A")]
+/// #[modifiers("guard_func0", "guard_func1")]
+/// ```
 #[proc_macro_attribute]
 pub fn modifiers(args: TokenStream, input: TokenStream) -> TokenStream {
     let args = parse_macro_input!(args as AttributeArgs);
